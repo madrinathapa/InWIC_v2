fd433c6e2efafd5d732992f14aff38d8737a2eb1
Prajna Peters <prajnacheryl@yahoo.co.in>
2017-06-24 12:29:07 -0700

    More new files

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$('.js-scroll').on('click', function(event) {
+  event.preventDefault();
+
+  var target = $(this).data('scrollTo');
+
+  $('html, body').animate({
+
+    scrollTop: $(target).offset().top
+    
+  }, 400);
+});
