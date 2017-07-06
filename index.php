132ec2e6c94eea5aa9889c3ae515329c87a347e8
Prajna Peters <prajnacheryl@yahoo.co.in>
2017-07-05 19:56:03 -0700

    Bug Fix - multiple contact mails sent on page refresh

--- index.php
+++ index.php
@@ -29,11 +29,18 @@
 
         // If there are no errors, send the email
         if (!$errName && !$errEmail && !$errMessage) {
+
             if (mail ($to, $subject, $body, $from)) {
-                $result='<div class="alert alert-success">Thank you! We will be in touch</div>';
-            } else {
+
+                $result='<div class="alert alert-success">Thank you! We will be in touch!</div>';
+            } 
+            else {
+                
                 $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
             }
+
+            //Clear all the feilds
+            $_POST=array(); 
         }
     }
 ?>
